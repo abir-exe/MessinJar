98ac5166643d154792db3517bdeb93ac2be545b6
Abir <abirmahmud221@gmail.com>
2024-03-21 13:18:28 +0600

    userController added

--- MessInJar-Server/backend/server.js
+++ MessInJar-Server/backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const cors = require('cors');
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes")
 const app = express();
 dotenv.config();
 
@@ -20,15 +21,18 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
+// app.get("/api/chat", (req, res) => {
+//   res.send(chats);
+// });
 
-app.get("/api/chat/:id", (req, res) => {
-  // console.log(req.params.id);
-  const singleChat = chats.find((chat) => chat._id === req.params.id);
-  res.send(singleChat);
-});
+// app.get("/api/chat/:id", (req, res) => {
+//   // console.log(req.params.id);
+//   const singleChat = chats.find((chat) => chat._id === req.params.id);
+//   res.send(singleChat);
+// });
+
+app.use('/api/user',userRoutes)
+
 
 const PORT = process.env.PORT || 5000;
 
